9b6b3c29f0d0f9ed995729f86d717ce622835da3
Matthew McKey <mc@staplez.io>
2014-02-28 16:29:23 -0600

    Auto Deploy

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,21 @@
+var fs = require('fs'),
+  shell =  require('child_process').exec,
+  child;
 module.exports = function(app) {
   app.get('*', function(req, res) {
       res.sendfile('./public/index.html');
     });
   app.post('/deploy', function(payload) {
-    console.log(payload.body);
+    var payer = payload.body.pusher;
+    if (payer.name == 'themcstaplez') {
+      child = exec('git pull',
+        function(error, stdout, stderr) {
+          console.log('stdout:' + stdout);
+          console.log('stderr:' + stderr);
+          if (error != null) {
+            console.log('exec error: ' + error)
+          }
+      });
+    }
   });
 };
