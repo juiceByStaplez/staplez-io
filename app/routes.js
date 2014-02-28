ac9e90a5b67602cf9b209090fb278c81645f6fd3
Matthew McKey <mc@staplez.io>
2014-02-28 16:02:47 -0600

    Test githook

--- app/routes.js
+++ app/routes.js
@@ -2,4 +2,7 @@ module.exports = function(app) {
   app.get('*', function(req, res) {
       res.sendfile('./public/index.html');
     });
+  app.post('/deploy', function(payload) {
+    console.log('payload');
+  });
 };
