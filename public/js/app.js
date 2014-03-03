7c174ee222094eeeb16ea184ae337bdd83e00d72
Matthew McKey <mc@staplez.io>
2014-03-03 10:52:06 -0600

    Contact form changes/implementation. Commit before moving local repo across directories

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
-var app = angular.module('sio', ['ngRoute', 'ngAnimate', 'appRoutes', 'sio.ctrl', 'sio.animations'])
+var app = angular.module('sio', ['ngRoute', 'ngAnimate', 'appRoutes', 'sio.ctrl'])
 
 angular.module('sio.ctrl', ['ctrl.main', 'ctrl.nav', 'ctrl.skills', 'ctrl.contact','ctrl.work', 'ctrl.user'])
--- public/js/controllers/contact.js
+++ public/js/controllers/contact.js
@@ -2,4 +2,10 @@ angular.module('ctrl.contact', [])
 .controller('contactCtrl', function($scope) {
   $scope.pageClass = 'contact';
   $scope.pageTitle = 'Contact';
+
+  $scope.submitForm = function() {
+    if ($scope.contactForm.$valid) {
+    alert('our form is amazing!');
+    }
+   };
 });
