40555d27093fdbb090ab64b26e85febfdc87eb98
Matthew McKey <mc@staplez.io>
2014-03-03 12:23:16 -0600

    Add UI Bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
-var app = angular.module('sio', ['ngRoute', 'ngAnimate', 'appRoutes', 'sio.ctrl'])
+var app = angular.module('sio', ['ngRoute', 'ngAnimate', 'ui.bootstrap','appRoutes', 'sio.ctrl'])
 
 angular.module('sio.ctrl', ['ctrl.main', 'ctrl.nav', 'ctrl.skills', 'ctrl.contact','ctrl.work', 'ctrl.user'])
