00cc5f950be631f41b5ce3022083487a0302d00b
Matthew McKey <mc@staplez.io>
2014-03-03 19:23:10 -0600

    Comment connect DB line

--- server.js
+++ server.js
@@ -11,7 +11,7 @@
 var db = require('./config/db');
 
   var port = process.env.PORT || 8080; // set our port
-  mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
+  // mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
 
   app.configure(function() {
     app.use(express.compress());
