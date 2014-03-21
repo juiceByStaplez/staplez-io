911c65a45f4cf4fb65f1fa6353e7d00e2b74d227
Matthew McKey <mc@staplez.io>
2014-03-20 23:50:54 -0500

    Added ng-Touch

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('sio', ['ngRoute', 'ngAnimate', 'appRoutes', 'sio.ctrl', 'sio.directives', 'sio.srv', 'sio.animations'])
+var app = angular.module('sio', ['ngRoute', 'ngAnimate', 'ngTouch'j, 'appRoutes', 'sio.ctrl', 'sio.directives', 'sio.srv', 'sio.animations'])
 
 angular.module('sio.ctrl', ['ctrl.main', 'ctrl.nav', 'ctrl.skills', 'ctrl.contact','ctrl.work', 'ctrl.user'])
 angular.module('sio.srv', ['srv.contact'])
