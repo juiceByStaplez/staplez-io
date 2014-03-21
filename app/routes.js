aa4b8306628d338933717b48aebe4fb06bb3d18b
Matthew McKey <mc@staplez.io>
2014-03-21 14:23:41 -0500

    Change routes.js

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ var fs = require('fs'),
 
 module.exports = function(app) {
   app.get('*', function(req, res) {
-      res.sendfile('../public/index.html');
+      res.sendfile('public/index.html');
     });
   app.post('/contact', function(req, res) {
     var formData = req.body;
