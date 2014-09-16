c0a240404a4435be40df53aee002b92d3cf6466f
Matthew McKey <mc@staplez.io>
2014-09-16 09:26:21 -0500

    Add homepage content

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,4 +2,6 @@ angular.module('ctrl.main', [])
 .controller('mainCtrl', function($scope) {
   $scope.pageClass = 'home';
   $scope.pageTitle = 'Home';
+
+  $scope.pageContent = 'Welcome to staplez.io! My online interactive portfolio';
 });
